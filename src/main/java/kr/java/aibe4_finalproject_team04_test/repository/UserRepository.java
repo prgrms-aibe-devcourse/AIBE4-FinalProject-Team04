d89aa944505783b3afa008b6c1130ac67ad742c7
godqhrenf <godqhrenf@gmail.com>
2026-02-10 09:11:45 +0900

    데모용 프로젝트 초기화

--- src/main/java/kr/java/aibe4_finalproject_team04_test/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package kr.java.aibe4_finalproject_team04_test.repository;
-
-import kr.java.aibe4_finalproject_team04_test.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
