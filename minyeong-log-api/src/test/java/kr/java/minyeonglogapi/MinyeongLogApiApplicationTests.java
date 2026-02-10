dfb29a90146145906337aa8127ac5acdd30c380d
godqhrenf <godqhrenf@gmail.com>
2026-02-10 12:35:12 +0900

    feat: [DOC-18] Spring Boot 프로젝트 초기화
    DOC-18 #time 0.5h

---
+++ minyeong-log-api/src/test/java/kr/java/minyeonglogapi/MinyeongLogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.java.minyeonglogapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinyeongLogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
