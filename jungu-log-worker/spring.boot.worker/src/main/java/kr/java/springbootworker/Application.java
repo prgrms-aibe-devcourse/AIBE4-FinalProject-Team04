ddfba973e861ef87c5ef77ed1e8670dfc52a1322
jk-Nam <skawnsrn94@gmail.com>
2026-02-10 15:32:10 +0900

    git commit -m "DOC-16 Spring Boot Worker 프로젝트 초기 세팅"

--- jungu-log-worker/spring.boot.worker/src/main/java/kr/java/springbootworker/Application.java
+++ jungu-log-worker/spring.boot.worker/src/main/java/kr/java/springbootworker/Application.java
@@ -2,7 +2,13 @@ package kr.java.springbootworker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableJpaAuditing
+@EnableScheduling
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class Application {
 
